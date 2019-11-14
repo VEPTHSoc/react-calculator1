21190831e1c5c9e672dd64814f85044e41af82c4
VEPTHSoc <42776975+VEPTHSoc@users.noreply.github.com>
2019-11-14 20:11:23 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from './containers/Calculator';
+
+class App extends React.Component{
+  render(){
+    return(
+      <Calculator />
+    )
+  }
+}
+
+export default App;
